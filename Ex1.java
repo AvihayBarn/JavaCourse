97a78d70f0883c90c6d49574c6813e200f7c217f
Avihay Barnholtz <57184849+AvihayBarn@users.noreply.github.com>
2020-11-12 17:57:40 +0200

    Add files via upload

--- Ex1.java
+++ Ex1.java
@@ -1,10 +1,20 @@
 
 
+import java.util.Scanner;
+
 public class Ex1 {
-	public static void main(int[] args) {
-		int a=args[0];
-		int b=args[1];
-		GPCD(a,b);
+	public static void main(String[] args) {
+		if(args[0]!=null) {
+			int a=Integer.parseInt(args[0]);
+			int b=Integer.parseInt(args[1]);
+			GPCD(a,b);
+		}
+		else {
+			Scanner s=new Scanner(System.in);
+			int a=s.nextInt();
+			int b=s.nextInt();
+			GPCD(a,b);
+		}
 	}
 	
 	//GCD function:
