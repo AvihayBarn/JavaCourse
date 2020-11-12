ffc4e83f9100b76da96ece93af9096287b8152b1
Avihay Barnholtz <57184849+AvihayBarn@users.noreply.github.com>
2020-11-12 17:44:43 +0200

    Add files via upload

---
+++ Ex1.java
@@ -0,0 +1,38 @@
+
+
+public class Ex1 {
+	public static void main(String[] args) {
+		int a=Integer.parseInt(args[0]);
+		int b=Integer.parseInt(args[1]);
+		GPCD(a,b);
+	}
+	
+	//GCD function:
+	public static void GPCD(int a,int b) {
+		int gcd;
+		if(a>b) gcd=GCD(a,b);
+		else gcd=GCD(b,a);
+		
+		if(isPrime(gcd)) System.out.println(gcd);
+		else {
+			int temp=0;
+			for(int i=2;i<=gcd-1;i++) {
+				if(a%i==0 && b%i==0 && isPrime(i)) temp=i;
+			}
+			System.out.println(temp);
+		}
+	}
+	public static int GCD(int a,int b) {
+		if(b==0) return a;
+		else return GCD(b,a%b);
+	}
+	public static boolean isPrime(int num) {
+		int count=0;
+		for(int i=1;i<num;i++) {
+			if(num%i==0) count++;
+		}
+		if(count==1) return true;
+		else return false;
+	}
+	
+}
